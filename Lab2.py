6826885609e79c3622034ebf710ccce750d1aab0
Janusz Siek <sieku358@gmail.com>
2020-04-14 08:17:38 +0200

    Zadanie 3

--- Lab2.py
+++ Lab2.py
@@ -14,7 +14,23 @@ while i < len(znaki):           #dodanie znakow do tablicy
 
 print("Podane znaki w odwrotnej kolejnosci: ", end="")
 tab.reverse()                   #odwrócenei tablicy
-i =0 ;
+i =0
 while i < len(tab):             #wypisanie znaków
     print(tab[i], end="")
     i += 1
+print()
+
+# Zadanie 3. Wypełniający tablicę liczbami losowymi rzeczywistymi z przedziału (-5,5). Wartość tablicy zapisz do pliku result.txt
+import random
+
+tab = array('i', [])                    #deklaracja tablicy
+for i in range(5):
+    tab.append(random.randint(-5,5))    #wypełnianie tablicy losowymi liczbami
+plik = open("result.txt","w")
+i = 0
+while i < len(tab):
+    plik.write(str(tab[i]) + " ")       #zapis do pliku
+    i += 1
+
+plik.close()
+
